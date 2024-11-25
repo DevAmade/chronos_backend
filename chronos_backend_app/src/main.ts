724b9b9a4d4ffcca3278263c236e3972bd1e6223
DevAmade <justin.dores@outlook.fr>
2024-11-25 19:23:38 +0100

    add server listen logging

--- chronos_backend_app/src/main.ts
+++ chronos_backend_app/src/main.ts
@@ -8,12 +8,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { logger: false });
     const configService = app.get(ConfigService);
+    const loggerService = app.get(WINSTON_MODULE_NEST_PROVIDER);
 
     app.setGlobalPrefix('api/chronos');
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
     app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(configService.get('APP_PORT'));
+
+    loggerService.log(
+        `Server listen: ${configService.get('APP_HOST')}:${configService.get('APP_PORT')}`,
+        'Server',
+    );
 }
 
 bootstrap();
