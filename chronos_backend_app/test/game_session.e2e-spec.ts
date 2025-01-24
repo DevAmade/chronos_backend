976fc96992f09166c27ced0a4c995dde43aa7d93
DevAmade <justin.dores@outlook.fr>
2025-01-24 11:56:59 +0100

    test actions

--- chronos_backend_app/test/game_session.e2e-spec.ts
+++ chronos_backend_app/test/game_session.e2e-spec.ts
@@ -129,7 +129,7 @@ describe('GameSessionController', () => {
                     host: process.env.DATABASE_HOST,
                     port: +process.env.DATABASE_PORT,
                     username: process.env.DATABASE_USERNAME,
-                    password: process.env.DATABASE_PASSWORD,
+                    password: 'postgres',
                     database: process.env.DATABASE_TEST_NAME,
                     logging: false,
                     models: [GameSession, GameSessionPlayer, Player, Game, Admin],
